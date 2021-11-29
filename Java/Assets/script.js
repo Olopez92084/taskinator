ae547e34cc5f226fbc35620d05fab61a0d0644d9
Orlando Lopez <94086814+Olopez92084@users.noreply.github.com>
2021-11-29 12:25:48 -0500

    Initial commit

---
+++ Java/Assets/script.js
@@ -0,0 +1,13 @@
+//Objects  are a collection of values
+
+var cars = {
+    make:"Honda",
+    model:"Civic",
+    year: 2003,
+    displayList: function () {
+        alert("Make: " + cars.make);
+    } 
+};
+
+cars.displayList();
+
